f3642efe19dd626f3bbbaa7d56903c7ad5c9e9b1
Daniel James <businessjames1999@gmail.com>
2020-02-06 14:30:07 -0500

    Added a few lines of code to program and tested for bugs

--- 19_Capstone/Capstone/Program.cs
+++ 19_Capstone/Capstone/Program.cs
@@ -11,16 +11,28 @@ namespace Capstone
             {
                 LoadVendingMachine();
 
-                Console.WriteLine("");
-                Console.WriteLine("");
-                Console.WriteLine("");
+                Console.WriteLine("1) Display Vending Machine Items");
+                Console.WriteLine("2) Purchase");
+                Console.WriteLine("3) Exit");
                 string input = Console.ReadLine();
 
+                switch (input)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        Console.WriteLine("eat pant");
+                        break;
+                }
             }
         }
         public static void LoadVendingMachine()
         {
-            VendingMachine vendingMachine = new VendingMachine("vendingmachine.csv");
+            VendingMachine vendingMachine = new VendingMachine(@"C:\Users\Student\git\c-module-1-capstone-team-7\19_Capstone\vendingmachine.csv");
 
         }
     }
